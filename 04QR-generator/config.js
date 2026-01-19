90810180c722df362e4b03c55d8c04b4156a4dc1
Nisha Bangali <nishabangali999@gmail.com>
2026-01-19 21:20:12 +0530

    change dbUrl

--- 04QR-generator/config.js
+++ 04QR-generator/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const DB_Connection = async () => {
     try {
-         await mongoose.connect("mongodb+srv://nisha:Re5Jp727ECHaxfkq@cluster0.6yblnj1.mongodb.net/mystorage?retryWrites=true&w=majority&appName=Cluster0")
+         await mongoose.connect("mongodb+srv://nishabangali214_db_user:aWqKx6CVtKUWtnzs@cluster0.y98iycl.mongodb.net/testStorage")
          console.log('MongoDB connected successfully')
         
     } catch (error) {
